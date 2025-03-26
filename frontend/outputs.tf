7edfa3c5d9401149e71afed65570e1b1092c54f1
Gal Halevi <93094499+gal-halevi@users.noreply.github.com>
2025-03-26 22:52:37 +0200

    Add apiUrl output

--- frontend/outputs.tf
+++ frontend/outputs.tf
@@ -4,3 +4,7 @@ output "cloudfront_urls" {
 output "cloudfront_dist_id" {
   value = module.cloudfront.distribution_id
 }
+
+output "BE_api_url" {
+  value = data.terraform_remote_state.backend.apiUrl
+}
