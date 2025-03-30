904a3a3c1656bb8d4769dd2adbe26644c3e66601
Gal Halevi <93094499+gal-halevi@users.noreply.github.com>
2025-03-30 22:07:58 +0300

    Add outputs

--- backend/stg/main.tf
+++ backend/stg/main.tf
@@ -1,5 +1,17 @@
 module "backend" {
   source = "../../modules/backend"
   env    = "stg"
-  api_cors_allowed_origins = [""]
+  api_cors_allowed_origins = ["*"]
+}
+
+output "apiUrl" {
+  value = module.backend.apiUrl
+}
+
+output "tableName" {
+  value = module.backend.tableName
+}
+
+output "primaryKey" {
+  value = module.backend.primaryKey
 }
